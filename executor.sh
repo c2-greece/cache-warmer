7ed57a34d9a923d14a90700029e95bfca1da6dc3
George Tarlas <g.tarlas@innotech.gr>
2022-04-10 17:51:02 +0300

    add option to convert sitemap urls to http for accessing  varnish directly

--- executor.sh
+++ executor.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 curl -s $SITEMAP | grep "<loc>" | awk -F"<loc>" '{print $2} ' | awk -F"</loc>" '{print $1}' > list.txt
+
+if [ "$CONVERTTOHTTP" = "tue" ]; then
+   sed -i 's|https://|http://|g' /list.txt
+fi
+
+
 siege -c1 -f list.txt
 exit 0
